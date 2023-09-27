9c804f65625212b78d0fe2e33b87bea7b0212984
CieranW <cieranwong@gmail.com>
2023-09-26 19:45:33 -0700

    Init commit: HW 8 Random Variables and Distribution Function
    Instructions for Part 3 - Practicing R have already been listed underneath their respective sections. Values haven't been included, might do that in a later commit
    
    Signed-off-by: CieranW <cieranwong@gmail.com>

---
+++ hw8.r
@@ -0,0 +1,17 @@
+# HW 8 - Random Variables and Distribution Functions
+# 3
+# A Gumbel random variable X has the distribution function F_x(x) = exp(-e^-x)
+
+# 3.1 - Calculate the First Quartile (Solve for F_x(x) = 0.25)
+
+# 3.2 - Distribution Function Graph
+# Plot graph of F_x(x), indicating the first and third quartile, and median as points on the graph
+
+# 3.3 - Probability Transform
+# Determine the probability transform for this distribution, writing x as a function of u
+
+# 3.4 - Simulating Gumbel Random Variables
+# Use the value found in 3.3 to create 80 samples with this distribution. Plot the graph that shows the true cumulative distribution function (in black) along with the emperical CDF of the data (different color)
+
+# 3.5 - Density Function
+# Determine the probability density function f_x(x) for the cumulative distribution function F_x(x)
